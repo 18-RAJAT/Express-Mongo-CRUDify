4a143528166a559cbde7a85aa0cbe278a4c4f767
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-08-09 10:29:42 +0530

    setup middleware and data check

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express=require('express');
 const app=express();
+const Product = require('./models/productModel')
 const mongoose=require('mongoose');
+//middleware
+app.use(express.json());
+
 //routes
 app.get('/',(req,res)=>{
     res.send('Hello World');
+});
+
+
+app.post('/products',async(req,res)=>{
+    try
+    {
+        const product=await Product.create(req.body);
+        res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.status(500).json({message:error.message});
+    }
 });
 
 mongoose.set('strictQuery', false);
