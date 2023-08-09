626560be5479094ff50fc4bdb4a43323edf4ae83
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-08-09 10:45:15 +0530

    fetch and Get Data from a Database

--- server.js
+++ server.js
@@ -10,6 +10,36 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
+//get data from database
+app.get('/products',async(req,res)=>{
+    try
+    {
+        const products=await Product.find({});
+        res.status(200).json(products);
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.status(500).json({message:error.message});
+    }
+});
+
+//specify id to get data from database
+app.get('/products/:id',async(req,res)=>{
+    try
+    {
+        const {id}=req.params;//.id;
+        // id=mongoose.Types.ObjectId(id);
+        // console.log(id);
+        const product=await Product.findById(req.params.id);
+        res.status(200).json(product);
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.status(500).json({message:error.message});
+    }
+});
 
 app.post('/products',async(req,res)=>{
     try
